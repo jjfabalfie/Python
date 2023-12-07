e9046c84d02c33279ee27bf438cbbc36547f3309
zXT1PP311Xz <126504278+zXT1PP311Xz@users.noreply.github.com>
2023-12-07 19:02:33 +0000

    Update JJFABSecurity.py

--- JJFABSecurity.py
+++ JJFABSecurity.py
@@ -1,16 +1,17 @@
-def main ():
+import os
+import getpass
+def main():
 	loginDB = {
 		"Alfie": "Password1",
 		"Jake": "Password2",
 		"Jasmine": "Password3",
-		"Bonnie": "Password4",
-	}
+		"Bonnie": "Password4",}
 	print("Welcome to JJFAB Security")
 	print("##########################")
-	user = str(input ("Please enter your username: "))
-	passwd = str(input("Please enter your password: "))
+	user = str(input("Please enter your username: "))
+	passwd = getpass.getpass(prompt="Password: ", stream=None) 
 	if user in loginDB and passwd == loginDB[user]:
 		print ("Login Succesful Welcome ",user, "!")
 	else:
 		print ("Login Failed, Locking Out")
-main ()
+main()
