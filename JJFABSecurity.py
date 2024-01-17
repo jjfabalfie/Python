a428f7573e0f566c4a6ace53920e6ade6aaf7e25
zXT1PP311Xz <126504278+zXT1PP311Xz@users.noreply.github.com>
2024-01-17 16:05:02 +0000

    Update JJFABSecurity.py

--- JJFABSecurity.py
+++ JJFABSecurity.py
@@ -2,10 +2,10 @@ import os
 import getpass
 def main():
 	loginDB = {
-		"Alfie": "Password1",
-		"Jake": "Password2",
-		"Jasmine": "Password3",
-		"Bonnie": "Password4",}
+		"User1": "Password1",
+		"User2": "Password2",
+		"User3": "Password3",
+		"User4": "Password4",}
 	print("Welcome to JJFAB Security")
 	print("##########################")
 	user = str(input("Please enter your username: "))
